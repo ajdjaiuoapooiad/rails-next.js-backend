07c865763a403ad1ae640331c05bf7878f30af61
yukito <iyggf66974@yahoo.ne.jp>
2025-02-01 13:01:07 +0900

    ok

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -9,11 +9,23 @@ class Api::V1::PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      render json: @post, status: :created
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :category, :income)
   end
 end
